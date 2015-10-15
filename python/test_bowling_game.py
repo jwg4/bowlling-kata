102f0d8c21878f8f265d306be8d703c4c10e531a
Jack <jack.grahl@yahoo.co.uk>
2015-10-15 07:23:03 +0100

    Factor out the method roll_many().

--- python/test_bowling_game.py
+++ python/test_bowling_game.py
@@ -6,13 +6,15 @@ class BowlingGameTest(unittest.TestCase):
     def setUp(self):
         self.g = Game()
 
+    def roll_many(self, n, pins):
+        for i in range(n):
+            self.g.roll(pins)
+
     def test_gutter_game(self):
-        for i in range(20):
-            self.g.roll(0)
+        self.roll_many(20, 0)
         self.assertEqual(self.g.score(), 0)
 
     def test_all_ones(self):
-        for i in range(20):
-            self.g.roll(1)
+        self.roll_many(20, 1)
         self.assertEqual(self.g.score(), 20)
 
