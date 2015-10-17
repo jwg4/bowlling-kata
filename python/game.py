8457a91bcb1ce905cd87e9caf02e47bc2470b417
Jack <jack.grahl@yahoo.co.uk>
2015-10-17 09:17:25 +0100

    New design - break the rolls into frames, then take the sum of the sums of frames.

--- python/game.py
+++ python/game.py
@@ -8,9 +8,14 @@ class Game(object):
     def score(self):
         total = 0
         count = len(self.scores)
-        for i in range(count):
-            s = self.scores[i]
-            total = total + s
-            if i + 2 < count and s + self.scores[i+1] == 10:
-                total = total + self.scores[i+2]
+        j = 0
+        frames = []
+        while j < count:
+            frame = self.scores[j:j+2]
+            if sum(frame) == 10:
+                frame.append(self.scores[j+2])
+            frames.append(frame)
+            j = j + 2
+        for f in frames:
+            total = total + sum(f)
         return total
