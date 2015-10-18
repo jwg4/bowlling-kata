2cd3ead7881aa4cfb090335799225f2d4daea29f
Jack <jack.grahl@yahoo.co.uk>
2015-10-19 00:15:37 +0100

    Recognize strikes and score them correctly.

--- python/game.py
+++ python/game.py
@@ -12,10 +12,15 @@ class Game(object):
         frames = []
         while j < count:
             frame = self.scores[j:j+2]
-            if sum(frame) == 10:
+            if frame[0] == 10:
                 frame.append(self.scores[j+2])
+                j = j + 1
+            elif sum(frame) == 10:
+                frame.append(self.scores[j+2])
+                j = j + 2
+            else:
+                j = j + 2
             frames.append(frame)
-            j = j + 2
         for f in frames:
             total = total + sum(f)
         return total
--- python/test_bowling_game.py
+++ python/test_bowling_game.py
@@ -33,3 +33,10 @@ class BowlingGameTest(unittest.TestCase):
         self.roll_many(17, 0)
         self.assertEqual(self.g.score(), 13)
 
+    def test_one_strike(self):
+        self.g.roll(10)
+        self.g.roll(5)
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        self.assertEqual(self.g.score(), 26)
+
