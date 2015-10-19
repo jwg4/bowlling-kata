fe65256e95212a8b321cb123ed76fcda9c9b5320
Jack <jack.grahl@yahoo.co.uk>
2015-10-19 21:05:17 +0100

    Clean up the if/elif/else statement.

--- python/game.py
+++ python/game.py
@@ -9,14 +9,17 @@ class Game(object):
         j = 0
         frames = []
         while len(frames) < 10:
-            frame = self.scores[j:j+2]
-            if frame[0] == 10:
-                frame.append(self.scores[j+2])
+            if self.scores[j] == 10:
+                # A strike: score for three rolls, start next frame after one
+                frame = self.scores[j:j+3]
                 j = j + 1
-            elif sum(frame) == 10:
-                frame.append(self.scores[j+2])
+            elif self.scores[j] + self.scores[j+1] == 10:
+                # A spare: score for three rolls, start next frame after two
+                frame = self.scores[j:j+3]
                 j = j + 2
             else:
+                # Neither: score for two rolls, start next frame after two
+                frame = self.scores[j:j+2]
                 j = j + 2
             frames.append(frame)
         return sum(sum(frame) for frame in frames)
