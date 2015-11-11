a43ef41a7d95d5fbe165851439430ece68bb1624
Jack <jack.grahl@yahoo.co.uk>
2015-11-11 23:37:25 +0000

    Rename test. (GREEN).

--- 2/test_bowling_game.py
+++ 2/test_bowling_game.py
@@ -3,7 +3,7 @@ import unittest
 from bowling_game import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
-    def test_game(self):
+    def test_score_before_any_rolls(self):
         game = BowlingGame()
         self.assertEqual(game.score(), 0)
 
