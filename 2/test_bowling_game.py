5236c1c7074ec8055b5498fd345771efa700a314
Jack <jack.grahl@yahoo.co.uk>
2015-11-11 23:38:26 +0000

    Add a test that rolling a number gets that score. (RED)

--- 2/test_bowling_game.py
+++ 2/test_bowling_game.py
@@ -7,3 +7,6 @@ class TestBowlingGame(unittest.TestCase):
         game = BowlingGame()
         self.assertEqual(game.score(), 0)
 
+    def test_single_roll(self):
+        game = BowlingGame()
+        game.roll(1)
