460067d447f94bf2303db57c6bbbe9340cb3e985
Jack <jack.grahl@yahoo.co.uk>
2015-11-14 22:22:32 +0000

    Check count_double before setting it as a result of that roll. (GREEN)

--- 2/bowling_game.py
+++ 2/bowling_game.py
@@ -2,17 +2,18 @@ class BowlingGame(object):
     def __init__(self):
         self._score = 0
         self._previous = 0
+        self.count_double = False
 
     def score(self):
         return self._score
 
     def roll(self, pins):
         self._score = self._score + pins
+        if self.count_double:
+            self._score = self._score + pins
         if pins + self._previous == 10:
             self.count_double = True
         else:
             self.count_double = False
-        if self.count_double:
-            self._score = self._score + pins
         self._previous = pins
 
