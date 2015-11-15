006c0fb139c453178da641e31cae591d8fdc539a
Jack <jack.grahl@yahoo.co.uk>
2015-11-15 10:37:42 +0000

    First attempt at handling strkes. (GREEN)

--- 2/bowling_game.py
+++ 2/bowling_game.py
@@ -17,6 +17,9 @@ class BowlingGame(object):
             self._end_of_frame = False
             if pins + self._previous == 10:
                 self._count_double = True
+        elif pins == 10:
+            self._count_double = True
+            self._end_of_frame = False
         else:
             self._end_of_frame = True
         self._previous = pins
